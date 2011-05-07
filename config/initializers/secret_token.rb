8f7100bcfffd805ba575bacdd965d2c1bd61018d
prakashmurthy <prakblr@yahoo.com>
2011-05-06 19:11:37 -0600

    First commit after 'rails new' and 'bundle install'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilearnedtoprogram::Application.config.secret_token = '83b50ab2612d4bf345c42e8e927d4ba9f8c3c43f9181d58b9603139d21b9278f89ebcaefffa6f067af205e0f615a8a361f92e66b6be543d846646cab9b342b11'
