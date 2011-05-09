26703b874ec05e21f62d0c4c90e9f2168f928368
prakashmurthy <prakblr@yahoo.com>
2011-05-09 15:43:39 -0600

    Apply the db migration for devise.

--- db/migrate/20110509213641_devise_create_users.rb
+++ db/migrate/20110509213641_devise_create_users.rb
@@ -2,11 +2,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
       t.database_authenticatable :null => false
-      t.recoverable
+      # t.recoverable
       t.rememberable
       t.trackable
 
-      # t.encryptable
+      t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
@@ -16,7 +16,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                :unique => true
-    add_index :users, :reset_password_token, :unique => true
+    # add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
