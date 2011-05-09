422a421cc45cd423875d7bc4b6a9fac911144284
prakashmurthy <prakblr@yahoo.com>
2011-05-09 16:06:41 -0600

    Add logout capability; disable signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :recoverable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
