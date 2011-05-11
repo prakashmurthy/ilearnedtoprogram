82a4c4d402d81c563e2b6e0c1cde36f3d822544b
prakashmurthy <prakblr@yahoo.com>
2011-05-10 22:10:25 -0600

    Modify the migration to populate the uuid field after creating it.

--- db/migrate/20110511035948_add_uuid_to_quote.rb
+++ db/migrate/20110511035948_add_uuid_to_quote.rb
@@ -2,6 +2,11 @@ class AddUuidToQuote < ActiveRecord::Migration
   def self.up
     add_column :quotes, :uuid, :string
     add_index :quotes, :uuid, :unique => true
+    
+    Quote.all.each do |quote|
+      quote.uuid = UUIDTools::UUID.timestamp_create.hexdigest
+      quote.save
+    end
   end
 
   def self.down
